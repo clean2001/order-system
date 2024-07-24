51fc41582e06a4f33e397faaa60278d142979f1c
clean2001 <clearrworld@gmail.com>
2024-07-24 17:29:05 +0900

    init: initial commit

---
+++ src/main/java/org/beyond/ordersystem/common/dto/SuccessResponse.java
@@ -0,0 +1,16 @@
+package org.beyond.ordersystem.common.dto;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class SuccessResponse extends CustomResponse {
+    String statusMessage;
+    Object result;
+
+    public SuccessResponse(HttpStatus httpStatus, String statusMessage, Object result) {
+        super(httpStatus.value());
+        this.statusMessage = httpStatus.getReasonPhrase();
+        this.result = result;
+    }
+}
