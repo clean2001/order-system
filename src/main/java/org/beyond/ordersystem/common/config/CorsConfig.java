cfb7e98650a60908941b5fa1e857238a63bfe585
Sejeong Kim <clearrworld@gmail.com>
2024-08-27 15:37:35 +0900

    chore: docker-compose세팅

--- src/main/java/org/beyond/ordersystem/common/config/CorsConfig.java
+++ src/main/java/org/beyond/ordersystem/common/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://www.clean01.store")
+                .allowedOrigins("http://www.clean01.store")
                 .allowedMethods("*").allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
